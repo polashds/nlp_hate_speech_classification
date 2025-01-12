f139cfc787e7f6678aa89b8472dab38aea9e563a
polashds <polashtechai@gmail.com>
2025-01-12 11:27:16 +0600

    venv created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hate-speech-classification",
+    version="0.0.0",
+    author="polashds",
+    author_email="polashtecha@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
